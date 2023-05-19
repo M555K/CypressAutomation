7462a37f60d4631c04ca267f549f892224d0ed75
Maria <kitmaria.2015@gmail.com>
2023-05-19 11:49:32 -0500

    add: assertions test

--- cypress/e2e/locators.cy.js
+++ cypress/e2e/locators.cy.js
@@ -30,5 +30,22 @@ describe('Fing or get Elements by using different locators',()=>{
         cy.get('button').should('contain', 'Login').click();
 
     })
+    it('Check finding elements by traveling through the DOM',()=>{
+        // travel to find the login button : 1-locate username box ; 2- go to parent form; 3- find the button
+        cy.get('input[name="username"]').parents('form').find('button').should('contain','Login').click();
+
+    })
+    it.only('Check different type of assertions',()=>{
+        // cypress bundless(uses) assertions provided by Chai. Sinon and jQuery libraries
+        //Should assertion
+        cy.get('#wooden_spoon').should('contain','Login')
+        .and('have.class','btn btn-primary');
+        //different approach: creates a subject of our test, then we can implement different actions
+        cy.get('#wooden_spoon').then((buttonElement)=>{
+            expect(buttonElement).to.have.text('Login');
+            expect(buttonElement).to.have.class('btn btn-primary');
+        })
+
+    })
    
 })
