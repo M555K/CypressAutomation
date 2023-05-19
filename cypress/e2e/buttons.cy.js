e7a69e5eccbc711dd8d14e7f06b71bbf42cc020b
Maria <kitmaria.2015@gmail.com>
2023-05-19 16:23:18 -0500

    add: add new button test

--- cypress/e2e/buttons.cy.js
+++ cypress/e2e/buttons.cy.js
@@ -12,6 +12,23 @@ describe('Fing or get Elements by using different locators',()=>{
         // select a button with text
         cy.contains('Button 2').should('be.visible').click();
         cy.contains('Clicked on button two!').should('be.visible');
-        
+        // find element with class attribute and create a list then select third element from the list
+        cy.get('.btn.btn-primary').then(($buttons)=>{
+            cy.wrap($buttons).eq(2).click();
+            cy.contains('Clicked on button three!').should('be.visible');
+            cy.get('button').each((item,index,list)=>{
+                expect(list).to.have.length(6);
+                expect(item).to.have.attr('onclick')
+                
+                })
+                //approach before not accurate
+                cy.get('button').each((item)=>{
+                    if(item.text() == "Button 4"){
+                        cy.log(item.text());// this command write the text at the test console
+                        cy.wrap(item).click();
+                        cy.contains('Clicked on button four!').should('be.visible');
+                    }
+            })
+        })
     })
     })
