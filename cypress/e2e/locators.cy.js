cd0281d8afa5c75c9f9317c72fdec34eca3f0141
Maria <kitmaria.2015@gmail.com>
2023-05-19 10:43:19 -0500

    add: more locators strategies

--- cypress/e2e/locators.cy.js
+++ cypress/e2e/locators.cy.js
@@ -11,11 +11,24 @@ describe('Fing or get Elements by using different locators',()=>{
     it('Check different locatators strategies',()=>{
         // by css locator
         cy.get("input[name='username']").type("CydeoStudent");
+
+        // tagName
         cy.get("[type='text']").clear();//clear what is typed
         cy.get('input').each((item,index,list)=>{
         expect(list).to.have.length(2);
         expect(item).has.attr("type");
         })
+
+        // attribute name
+        cy.get('[type]');
+        // class attribute value or className
+        cy.get('.btn.btn-primary');
+
+        // by id
+        cy.get('#wooden_spoon');
+        // if I want to use text: no xpath  in cypress, but it still posisble
+        cy.get('button').should('contain', 'Login').click();
+
     })
    
 })
