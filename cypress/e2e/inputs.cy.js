09607232089017ff4479432728173287d4ac21ce
Maria <kitmaria.2015@gmail.com>
2023-05-24 14:51:47 -0500

    add: new tests wich file inputs tests

--- cypress/e2e/inputs.cy.js
+++ cypress/e2e/inputs.cy.js
@@ -1,15 +1,49 @@
 /// <reference types="cypress" />
 
 describe('Input Form Tests', () => {
-    beforeEach('Navigate to registration page',() => {
-      // runs before each test case, beforeMethod in TestNG
-      cy.clearCookies();//cypress do it automatically, we can avoid it
-      cy.get('/registration_form');
-    });
-  
-    it('Check Different Input Box fields and verify', () => {
-        
-        });
-      
+  beforeEach('Navigate to registration page', () => {
+    // runs before each test case, beforeMethod in TestNG
+    cy.clearCookies(); // cypress do it automatically, we can avoid it
+    cy.visit('/registration_form');
   });
-  
+
+  it.skip('Check Different Input Box fields and verify', () => {
+    cy.get('input[name="firstname"]').type('Mike');
+    cy.get('input[name="lastname"]').type('Braun');
+    cy.get('input[name="username"]').type('believer');
+    const email = `formtest${Math.floor(100000 + Math.random() * 900000)}@cydeo.com`;
+    /**
+     * Math.random() creates a number between 0 and 1 as ex 0.000234
+     * Math.floor == makes it a whole number
+     */
+
+    cy.get('input[name="email"]').type(email);
+    const password = `test${Math.floor(100000 + Math.random() * 900000)}`;
+    cy.get('input[name="password"]').type(password);
+    const phoneNumber = `555-000-${Math.floor(1000 + Math.random() * 9000)}`; // creates 4 digits extension
+    cy.get('input[name="phone"]').type(phoneNumber);
+    cy.get('input[name="birthday"]').type('01/05/1995');
+  });
+  it.skip('Check different radio buttons actions',() => {
+     /** 
+      * radio is Jquery element , cy.wrap(radio) turns it to the cypress object so that i can use cypres functions
+      * first() select first element
+     */
+    cy.get('.radio').find('[type=radio]').then((radio => {
+        cy.wrap(radio).first().check().should('be.checked');
+        // select the second radio buttons and verify it is checked and confirmation lable is visible
+        cy.wrap(radio).eq(1).check().should('be.checked');
+        cy.get('[data-bv-icon-for="gender"]').should('be.visible');
+        cy.wrap(radio).eq(2).should('not.be.checked');
+    }));
+  });
+  it('Check different checkbox actions',()=>{
+    cy.get('[type="checkbox"]').then((checkbox) =>{
+        cy.wrap(checkbox).eq(1).check().should('be.checked');
+        // unchecked java
+        cy.wrap(checkbox).eq(1).uncheck().should('not.be.checked');
+    })
+  })
+
+
+});
